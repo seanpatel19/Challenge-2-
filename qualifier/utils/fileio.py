65514e30e09f13a1fb36a0c8e97915c2a2c34389
seanpatel19 <84413793+seanpatel19@users.noreply.github.com>
2021-07-11 23:18:10 -0400

    Add files via upload
    This version has gone through significant changes, and it also has been moved to another repository due to issues with the first one. The changes are as follows: a save_csv function has been added to save the output of app.py, and a save_qualifying_loans function has been created to ask the user if they wish to save the output of the function to a folder called output within data. Testing has also been added to the app."

---
+++ qualifier/utils/fileio.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""Helper functions to load and save CSV data.
+
+This contains a helper function for loading and saving CSV files.
+
+"""
+import csv
+from pathlib import Path
+
+
+def load_csv(csvpath):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        next(csvreader)
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+
+# moved save_csv function from app.py to here 
+def save_csv(csvpath, data, header=None):
+    with open(csvpath, "w", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        if header:
+            csvwriter.writerow(header)
+        csvwriter.writerows(data)
+    csvpath= Path("./data/output/qualifying_loans.csv")
